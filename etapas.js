558bc6d971e0ff98839a44c7aea11f1b62eb10df
mariaandreza <mandreza008@gmail.com>
2023-04-26 01:59:09 -0300

    projeto urna eletronica

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: "fulano de tal",
+                partido: "ABC",
+                fotos: [
+                    {url: '381111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: "beltrano de tal",
+                partido: "DEF",
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: "Ciclano",
+                partido: "ABC",
+                vice: "Cic",
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeiro'},
+                    {url: '77222.jpg', legenda: 'Vice', small: false}
+                ]
+            },
+            {
+                numero: '84',
+                nome: "Zulano",
+                partido: "FDDS",
+                vice: "Zul",
+                fotos: [
+                    {url: '381111.jpg', legenda: 'Prefeiro'},
+                    {url: '99.jpg', legenda: 'Vice-Prefeito', small: false}
+                ]
+            },
+            
+
+        ]
+    },
+
+]; 
